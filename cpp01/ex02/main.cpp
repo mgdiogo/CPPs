467b53c5125e4f01ce629c4979c80368142d0c21
Miguel Diogo <miguel.g.diogo@gmail.com>
2023-11-26 15:00:01 +0000

    cpp01 `ex02` resolution

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 14:48:22 by mpedroso          #+#    #+#             */
+/*   Updated: 2023/11/26 14:58:23 by mpedroso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string string = "HI THIS IS BRAIN.";
+	std::string *stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory address of the string variable -> " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR -> " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF -> " << &stringREF << std::endl;
+	std::cout << "\n";
+	std::cout << "Value of the string variable  -> " << string << std::endl;
+	std::cout << "Memory pointed to by stringPTR -> " << *stringPTR << std::endl;
+	std::cout << "Memory pointed to by stringREF -> " << stringREF << std::endl;
+}
