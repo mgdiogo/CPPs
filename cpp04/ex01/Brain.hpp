ca6b5b3f3b45d69cab3ef08927ab87480738e1c7
Miguel Diogo <miguel.g.diogo@gmail.com>
2024-03-10 16:17:56 +0000

    removed unnecessary private keyword

--- cpp04/ex01/Brain.hpp
+++ cpp04/ex01/Brain.hpp
@@ -6,7 +6,7 @@
 /*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 17:55:22 by mpedroso          #+#    #+#             */
-/*   Updated: 2024/03/06 18:08:18 by mpedroso         ###   ########.fr       */
+/*   Updated: 2024/03/10 16:09:01 by mpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 #include <iostream>
 
 class Brain {
-	private:
 	public:
 		Brain(void);
 		Brain(const Brain &cpy);
