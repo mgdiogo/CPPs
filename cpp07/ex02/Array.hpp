ccb633d9cf309d27d7804b6e026564cd929d2767
Miguel Diogo <miguel.g.diogo@gmail.com>
2024-11-28 18:58:42 +0000

    ex02 resolution

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:20:10 by mpedroso          #+#    #+#             */
-/*   Updated: 2024/11/26 15:55:55 by mpedroso         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:58:26 by mpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,21 @@ class Array {
 		T *arr;
 		int _size;
 	public:
-		
 		Array();
 		Array(unsigned int n);
 		Array(const Array &cpy);
-		Array& operator=(const Array &cpy);
+		T& operator[](unsigned int i);
+		T& operator=(const Array &cpy);
 		~Array();
-		int size(void) const;
+		unsigned int size(void) const;
+		void setValues(void);
 };
 
 template <class T>
 
 Array<T>::Array() {
-	this-> arr = nullptr;
 	_size = 0;
+	this-> arr = NULL;
 }
 
 template <class T>
@@ -49,14 +50,44 @@ Array<T>::Array(unsigned int n) {
 
 template <class T>
 
+Array<T>::Array(const Array &cpy) {
+	_size = cpy.size();
+	this->arr = NULL;
+	if (cpy.arr) {
+		arr = new T[cpy.size()];
+		for (unsigned int i = 0; i < cpy.size(); ++i) {
+			this->arr[i] = cpy.arr[i];
+		}
+	}
+}
+
+template <class T>
+
 Array<T>::~Array() {
 	delete[] arr;
 }
 
 template <class T>
 
-int Array<T>::size(void) const {
+void Array<T>::setValues(void) {
+	for (unsigned int i = 0; i < size(); ++i) {
+		std::cin >> this->arr[i];
+	}
+}
+
+template <class T>
+
+unsigned int Array<T>::size(void) const {
 	return (_size);
 } 
+
+template <class T>
+
+T& Array<T>::operator[](unsigned int i){
+	if (i > size()) {
+		throw std::out_of_range("Tried to access an invalid index inside the array!");
+	}
+	return (this->arr[i]);
+}
 
 #endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 15:28:24 by mpedroso          #+#    #+#             */
-/*   Updated: 2024/11/26 15:54:10 by mpedroso         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:53:25 by mpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,66 @@
 
 int main(void) {
 	Array<int> a(5);
+	std::cout << "Set array values below: " << std::endl;
+	a.setValues();
+	Array<int> b(a);
 	
-	std::cout << a.size() << std::endl;
+	std::cout << "Array A -> ";
+	for (unsigned int i = 0; i < a.size(); ++i) {
+		if (i == 0)
+			std::cout << "{ ";
+		std::cout << a[i];
+		if (i == a.size() - 1) {
+			std::cout << " }" << std::endl;
+			break ;
+		}
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Array B -> ";
+	for (unsigned int i = 0; i < b.size(); ++i) {
+		if (i == 0)
+			std::cout << "{ ";
+		std::cout << b[i];
+		if (i == b.size() - 1) {
+			std::cout << " }" << std::endl;
+			break ;
+		}
+		std::cout << " ";
+	}
+	std::cout << "Change array b values: " <<std::endl;
+	b.setValues();
 	
+	std::cout << "Array A -> ";
+	for (unsigned int i = 0; i < a.size(); ++i) {
+		if (i == 0)
+			std::cout << "{ ";
+		std::cout << a[i];
+		if (i == a.size() - 1) {
+			std::cout << " }" << std::endl;
+			break ;
+		}
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	std::cout << "Array B -> ";
+	for (unsigned int i = 0; i < b.size(); ++i) {
+		if (i == 0)
+			std::cout << "{ ";
+		std::cout << b[i];
+		if (i == b.size() - 1) {
+			std::cout << " }" << std::endl;
+			break ;
+		}
+		std::cout << " ";
+	}
+	
+	std::cout << "** Out of bounds test **" << std::endl;
+	try {
+		std::cout << a[9] << std::endl;
+	}
+	catch (std::out_of_range &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
