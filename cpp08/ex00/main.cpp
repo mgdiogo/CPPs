5135aea1a57dc0c3487e5312dffd6d0a79364437
Miguel Diogo <miguel.g.diogo@gmail.com>
2024-12-05 20:28:23 +0000

    Solved a grammar issue

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mpedroso <mpedroso@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 19:51:00 by mpedroso          #+#    #+#             */
-/*   Updated: 2024/12/05 20:15:01 by mpedroso         ###   ########.fr       */
+/*   Updated: 2024/12/05 20:27:52 by mpedroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(void) {
 	vec[4] = 55;
 	
 	try {
-		std::cout << "Finding the number inside the container will only print himself, while not finding it will throw an exception!" << std::endl;
+		std::cout << "Finding the number inside the container will only print itself, while not finding it will throw an exception!" << std::endl;
 		std::cout << easyfind(vec, 43) << std::endl;
 	}
 	catch (std::runtime_error &e) {
